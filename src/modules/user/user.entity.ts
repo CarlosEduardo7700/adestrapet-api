8f42c1587143ce35081a796bd3c578ac5fb28d33
CarlosEduardo0077 <mendoncaeduardo652@gmail.com>
2025-10-01 21:28:42 -0300

    feat: Maps the User entity to the database

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -1,1 +1,50 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'name', length: 200, nullable: false })
+  name: string;
+
+  @Column({ name: 'email', length: 255, nullable: false, unique: true })
+  email: string;
+
+  @Column({ name: 'password', length: 100, nullable: false, select: false })
+  password: string;
+
+  @Column({ name: 'streaks', nullable: false, type: 'integer' })
+  streaks: number;
+
+  @Column({
+    name: 'last_access',
+    nullable: false,
+    type: 'timestamp with time zone',
+  })
+  lastAccess: Date;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    nullable: false,
+    type: 'timestamp with time zone',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    nullable: false,
+    type: 'timestamp with time zone',
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at', type: 'timestamp with time zone' })
+  deletedAt: Date;
+}
