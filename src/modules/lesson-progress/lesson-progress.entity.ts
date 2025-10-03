9d72ed2f70f7d74b3648d756e29d72230474c05c
CarlosEduardo0077 <mendoncaeduardo652@gmail.com>
2025-10-03 20:59:15 -0300

    feat: Maps the LessonProgress entity to the database

--- src/modules/lesson-progress/lesson-progress.entity.ts
+++ src/modules/lesson-progress/lesson-progress.entity.ts
@@ -1,1 +1,45 @@
-export class LessonProgress {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'lesson_progress' })
+export class LessonProgress {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'is_fav', nullable: false })
+  isFav: boolean;
+
+  @Column({
+    name: 'last_access',
+    nullable: false,
+    type: 'timestamp with time zone',
+  })
+  lastAccess: Date;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    nullable: false,
+    type: 'timestamp with time zone',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    nullable: false,
+    type: 'timestamp with time zone',
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    type: 'timestamp with time zone',
+    nullable: true,
+  })
+  deletedAt: Date;
+}
