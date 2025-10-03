2c68b61f55659b7a67f9af5553332bddc2461cd4
CarlosEduardo0077 <mendoncaeduardo652@gmail.com>
2025-10-02 22:35:59 -0300

    feat: Maps the Lesson entity to the database

--- src/modules/lesson/lesson.entity.ts
+++ src/modules/lesson/lesson.entity.ts
@@ -1,1 +1,53 @@
-export class Lesson {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'lessons' })
+export class Lesson {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'title', length: 255, nullable: false })
+  title: string;
+
+  @Column({ name: 'description', type: 'text', nullable: true })
+  description: string;
+
+  @Column({ name: 'video_url', length: 500, nullable: false, unique: true })
+  videoUrl: string;
+
+  @Column({ name: 'logo_url', length: 500, nullable: true })
+  logoUrl: string;
+
+  @Column({ name: 'support_material_url', length: 500, nullable: true })
+  supportMaterialUrl: string;
+
+  @Column({ name: 'duration', type: 'integer', nullable: false })
+  duration: number;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    nullable: false,
+    type: 'timestamp with time zone',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    nullable: false,
+    type: 'timestamp with time zone',
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    type: 'timestamp with time zone',
+    nullable: true,
+  })
+  deletedAt: Date;
+}
