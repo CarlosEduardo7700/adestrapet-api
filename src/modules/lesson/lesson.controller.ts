cb805c6b58a12f479de845a2ba120461a5c2a159
CarlosEduardo0077 <mendoncaeduardo652@gmail.com>
2025-10-06 21:17:12 -0300

    feat: Creates an endpoint to get a Lessons list

--- src/modules/lesson/lesson.controller.ts
+++ src/modules/lesson/lesson.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { LessonService } from './lesson.service';
+import { Lesson } from './lesson.entity';
 
 @Controller('lesson')
 export class LessonController {
   constructor(private readonly lessonService: LessonService) {}
+
+  @Get()
+  async getLessons() {
+    const lessons: Lesson[] = await this.lessonService.getLessons();
+    return {
+      message: `Quantidade de aulas encontradas: ${lessons.length}.`,
+      data: lessons,
+    };
+  }
 }
--- src/modules/lesson/lesson.service.ts
+++ src/modules/lesson/lesson.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { Lesson } from './lesson.entity';
 
 @Injectable()
-export class LessonService {}
+export class LessonService {
+  getLessons(): Promise<Lesson[]> {
+    throw new Error();
+  }
+}
