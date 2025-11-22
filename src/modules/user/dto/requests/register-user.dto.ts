d5cef16fb20902a26a57cb2b68ce242b4670f54d
CarlosEduardo0077 <mendoncaeduardo652@gmail.com>
2025-11-21 22:31:21 -0300

    feat: Adds validations in RegisterUserDto

--- src/modules/user/dto/requests/register-user.dto.ts
+++ src/modules/user/dto/requests/register-user.dto.ts
@@ -1,5 +1,30 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
+
+const passwordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/;
+
 export class RegisterUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
   name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @Matches(passwordRegex, {
+    message:
+      'The password must contain at least a uppercase letter, a lowercase, a number and a special character (!@#$%^&*).',
+  })
   password: string;
 }
